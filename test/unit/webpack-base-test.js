af484c8d3bc82710d7f897923d6e9b2ee250c19c
Andy Sun <yiensuen@gmail.com>
2020-12-03 14:56:27 +1100

    feat:add travis file

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,6 +3,9 @@ describe("webpack.base.js test case", () => {
   const baseConfig = require("../../lib/webpack.base");
   console.log(baseConfig);
   it("entry", () => {
-    assert.equal(baseConfig.entry.index, "");
+    assert.equal(
+      baseConfig.output.path,
+      "/Users/andysun/Desktop/webpack-study/builder-webpack/test/smoke/template/dist"
+    );
   });
 });
